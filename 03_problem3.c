ec711bf36997b6ab256234265b0c7f74acd5e207
Ishwarya S <sureshishwarya666@gmail.com>
2025-09-03 23:17:00 +0530

    Update 03_problem3.c

--- 03_problem3.c
+++ 03_problem3.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 int main(){
     int x=2, y=3, z=3, k=1;
-    int b = 3*x/y-z+k; //priority given according to operators precedence
+    int b = 3*x/y-z+k; //priority given according to operator precedence
     printf("The value of b is %d", b);
 return 0;
 }
+//output
+// 3*2/y-z+k
+//6/3-z+k
+//2-3+k
+//-1+1
+//0
